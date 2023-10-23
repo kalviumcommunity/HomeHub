1a79d4fa1103da37682b35dff447365e0ab4e3e6
krittikatiwari <113347197+krittikatiwari@users.noreply.github.com>
2023-10-23 21:11:31 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,25 @@
+use estate;
+
+SELECT * FROM Users;
+
+SELECT * FROM Roles;
+
+SELECT * FROM UserRoles;
+
+SELECT * FROM Agents;
+
+SELECT * FROM Clients;
+
+SELECT * FROM Owners;
+
+SELECT * FROM Addresses;
+
+SELECT * FROM Properties;
+
+SELECT * FROM Transactions;
+
+SELECT * FROM Commission;
+
+SELECT * FROM PropertyImages;
+
+SELECT * FROM Booking;
